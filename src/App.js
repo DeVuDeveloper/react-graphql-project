33c9e52a154fa45adb4c94b73deae8994f8a7b42
VuDej <drvu47@gmail.com>
2022-07-08 16:25:29 +0200

    readme update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './Style.css';
+
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from "react-apollo";
+import Launches from "./components/Launches";
+import Launch from './components/Launch';
+import { Router } from "@reach/router";
+
+const client = new ApolloClient({
+	uri: 'http://localhost:5000/graphql'
+});
+
+class App extends React.Component {
+	render() {
+		return(
+			<ApolloProvider client={client}>
+				<div className="container">
+					
+					<Router>
+						<Launches path="/"/>
+						<Launch path="/launch/:id"/>
+					</Router>
+				</div>
+			</ApolloProvider>
+
+		)
+	}
 }
 
 export default App;
